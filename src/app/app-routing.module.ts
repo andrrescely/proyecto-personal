c2b8aa147103575cecb0199c541f84b9a72325dd
andrrescely <134743627+andrrescely@users.noreply.github.com>
2024-02-22 00:52:19 -0500

    implement login and 4 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'section1', component: Section1Component },
+  { path: 'section2', component: Section2Component },
+  { path: 'section3', component: Section3Component },
+  { path: 'section4', component: Section4Component },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirecciona al home por defecto
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/home' }, // Redirecciona a home si la ruta no es válida
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
